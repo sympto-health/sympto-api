6ae27ea63c4687705ffabda9154f006a3c253caa
Prithvi Narasimhan <narasimhan.prithvi@gmail.com>
2019-11-22 08:44:47 -0800

    Fix error handling

--- sympto-api/src/Server.ts
+++ sympto-api/src/Server.ts
@@ -1,8 +1,11 @@
+require('express-async-errors');
+
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import { Request, Response } from 'express';
 import logger from 'morgan';
 import path from 'path';
+import { BAD_REQUEST } from 'http-status-codes';
 import BaseRouter from './routes';
 
 // Init express
@@ -15,6 +18,11 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', BaseRouter);
+app.use((err: Error, req: Request, res: Response, next: Function) => {
+  console.error(err.stack)
+  res.status(BAD_REQUEST).send(err.message)
+});
+
 
 // Export express instance
 export default app;
--- sympto-api/src/routes/index.ts
+++ sympto-api/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 import { BAD_REQUEST, OK } from 'http-status-codes';
 import axios from 'axios';
 import { clientId, clientSecret, clientURL } from '../config';
+
 import logger from '../logger';
 
 // Init router and path
@@ -10,25 +11,25 @@ const router = Router();
 console.log({ clientId, clientSecret, clientURL } );
 
 /**
+ * Webhook used as clinic endpoint
+ */
+router.post('/webhook', async (req: Request, res: Response) => {
+  logger.info('Request from Sympto Health');
+});
+
+/**
  * Generate auth code and create a set of users and groups
  */
 router.get('/setup', async (req: Request, res: Response) => {
-  try {
-    logger.info('Fetching auth code');
+  logger.info('Fetching auth code');
 
-    // Generates auth code
-    const { data: { Response: authCode } } = await axios.post(`${clientURL}/authorization`, {
-      clientId,
-      clientSecret,
-    });
-    logger.info(`Auth code is ${authCode}`);
-
-
-  } catch (err) {
-    return res.status(BAD_REQUEST).json({
-      error: err.message,
-    });
-  }
+  // Generates auth code
+  const { data: { Response: authCode } } = await axios.post(`${clientURL}/authorization`, {
+    clientId,
+    clientSecret,
+  });
+  logger.info(`Auth code is ${authCode}`);
+  throw new Error('wow');
 });
 
 router.get('/users', async (req: Request, res: Response) => {
