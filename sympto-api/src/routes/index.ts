f6ab0174fe4c38a64ad9724ee6a42901660b6034
Prithvi Narasimhan <narasimhan.prithvi@gmail.com>
2019-11-22 08:34:37 -0800

    auth endpoint

--- sympto-api/src/routes/index.ts
+++ sympto-api/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 import { BAD_REQUEST, OK } from 'http-status-codes';
 import axios from 'axios';
 import { clientId, clientSecret, clientURL } from '../config';
+import logger from '../logger';
 
 // Init router and path
 const router = Router();
@@ -12,12 +13,46 @@ console.log({ clientId, clientSecret, clientURL } );
  * Generate auth code and create a set of users and groups
  */
 router.get('/setup', async (req: Request, res: Response) => {
-  // Generates auth code
-  const authCode = await axios.post('/authorization', {
-    clientId,
-    clientSecret,
-  });
-  res.send(authCode);
+  try {
+    logger.info('Fetching auth code');
+
+    // Generates auth code
+    const { data: { Response: authCode } } = await axios.post(`${clientURL}/authorization`, {
+      clientId,
+      clientSecret,
+    });
+    logger.info(`Auth code is ${authCode}`);
+
+
+  } catch (err) {
+    return res.status(BAD_REQUEST).json({
+      error: err.message,
+    });
+  }
+});
+
+router.get('/users', async (req: Request, res: Response) => {
+  try {
+    logger.info('Fetching auth code');
+
+    // Generates auth code
+    const { data: { Response: authCode } } = await axios.post(`${clientURL}/authorization`, {
+      clientId,
+      clientSecret,
+    });
+    logger.info(`Auth code is ${authCode}`);
+
+    // Create 3 groups
+    const { data: { Response: groupId } } = await axios.post(`${clientURL}/clinicAdmin/groups`, {
+      groupName: 'Group 1',
+    });
+    res.send(authCode);
+
+  } catch (err) {
+    return res.status(BAD_REQUEST).json({
+      error: err.message,
+    });
+  }
 });
 
 /***
